c47174b1a816bf02feb5fe18135bf4a2d747b091
mahdipratama <mahdipratama29@gmail.com>
2023-04-02 12:20:41 +0700

    Forecast API integration with Hourly and 5 Day forecast section and fix loading display

--- app/js/app.js
+++ app/js/app.js
@@ -109,8 +109,8 @@ const errorContent = document.querySelector('[data-error-content]');
  */
 export const updateWeather = async function (lat, lon) {
 
-  // loading.style.display = 'grid';
-  // container.style.overflowY = 'hidden';
+  loading.style.display = 'grid';
+  container.style.overflowY = 'hidden';
   container.classList.remove('fade-in');
   errorContent.style.display = 'none';
 
@@ -137,7 +137,7 @@ export const updateWeather = async function (lat, lon) {
 
   const [{ name, country }] = await fetchData(url.reverseGeo(lat, lon))
 
-  console.log(currentWeather);
+  // console.log(currentWeather);
 
   const {
     weather,
@@ -338,6 +338,134 @@ export const updateWeather = async function (lat, lon) {
   `;
 
   highlightSection.appendChild(cardHighlights);
+
+
+  //////////////////////////
+  // 24H FORECAST SECTION
+  const forecast = await fetchData(url.forecast(lat, lon));
+
+  const {
+    list: forecastList,
+    city: { timezoneForecast },
+  } = forecast;
+
+  hourlySection.innerHTML = `
+    <h2 class="title-2">Today at</h2>
+
+    <div class="slider-container">
+      <ul class="slider-list" data-temp></ul>
+
+      <ul class="slider-list" data-wind></ul>
+    </div>
+  `;
+
+  forecastList.forEach((data, index) => {
+    if (index > 7) return;
+
+    const {
+      dt: dateTimeUnix,
+      main: { temp },
+      weather,
+      wind: { deg: windDirection, speed: windSpeed }
+    } = data;
+
+    const [{ icon, description }] = weather;
+
+    const tempLi = document.createElement('li');
+    tempLi.classList.add('slider-item');
+
+    tempLi.innerHTML = `
+    <div class="card card-sm slider-card">
+      <p class="body-3">
+        ${module.getHours(dateTimeUnix, timezone)}
+      </p>
+      <img
+          src="/app/icons/${icon}.png" alt="${description}"
+          width="48"
+          height="48"
+          loading="lazy"
+          class="weather-icon"
+          title="${description}">
+      <p class="body-3">${parseInt(temp)}&deg;</p>
+    </div>
+    `;
+
+    hourlySection.querySelector('[data-temp]').appendChild(tempLi);
+
+    const windLi = document.createElement('li');
+    windLi.classList.add('slider-item');
+
+    windLi.innerHTML = `
+    <div class="card card-sm slider-card">
+      <p class="body-3">${module.getHours(dateTimeUnix, timezone)}</p>
+      <img
+          src="/app/icons/direction.png" alt="overcast clouds"
+          width="48"
+          height="48"
+          loading="lazy"
+          class="weather-icon"
+          style="transform: rotate(${windDirection - 180}deg)"
+          title="overcast clouds">
+      <p class="body-3">${parseInt(module.mps_to_kmh(windSpeed))} km/h</p>
+    </div>
+    `;
+
+    hourlySection.querySelector('[data-wind]').appendChild(windLi);
+
+
+    //////////////////////////
+    // 5 DAY FORECAST SECTION
+    forecastSection.innerHTML = `
+    <h2 class="title-2" id="forecast-label">
+      5 Days Forecast
+    </h2>
+
+    <div class="card card-lg forecast-card">
+      <ul data-forecast-list></ul>
+    </div>
+  `;
+
+
+    for (let i = 7, len = forecastList.length; i < len; i += 8) {
+
+      const {
+        main: { temp_max },
+        weather,
+        dt_txt
+      } = forecastList[i];
+
+      const [{ icon, description }] = weather;
+      const date = new Date(dt_txt);
+
+      const li = document.createElement('li');
+      li.classList.add('card-item');
+
+      li.innerHTML = `
+      <p class="label-1">${date.getDate()} ${module.monthNames[date.getUTCMonth()]}</p>
+      <div class="icon-wrapper">
+        <img
+             src="./app/icons/${icon}.png"
+             width="36"
+             height="36"
+             alt="${description}" 
+             class="weather-icon"
+             title="${description}">
+        <span class="title-2">
+        ${parseInt(temp_max)}&deg;
+        </span>
+      </div>
+      <p class="label-1">${module.weekDayNames[date.getUTCDay()]}</p>
+      `;
+
+      forecastSection.querySelector('[data-forecast-list]').appendChild(li);
+
+    }
+
+    loading.style.display = 'none';
+    container.style.overflowY = 'overlay';
+    container.classList.remove('fade-in');
+
+  });
 
 };
 
